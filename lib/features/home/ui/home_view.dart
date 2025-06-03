bf429e0c71a87cb7112b9421fac42eb46971c0e4
EngAhmed20 <ahmed2012muhammad@gmail.com>
2025-06-03 18:25:06 +0300

    product card

--- lib/features/home/ui/home_view.dart
+++ lib/features/home/ui/home_view.dart
@@ -44,6 +44,7 @@ class HomeView extends StatelessWidget {
         ),
         /// product card
         Card(
+
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10.h),
           ),

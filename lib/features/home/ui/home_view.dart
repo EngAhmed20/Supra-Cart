3487edcb2526d48d7824c6af9a9e9db309b109e9
EngAhmed20 <ahmed2012muhammad@gmail.com>
2025-06-04 17:44:51 +0300

    product card ui

--- lib/features/home/ui/home_view.dart
+++ lib/features/home/ui/home_view.dart
@@ -44,7 +44,6 @@ class HomeView extends StatelessWidget {
         ),
         /// product card
         Card(
-
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10.h),
           ),
@@ -89,10 +88,7 @@ class HomeView extends StatelessWidget {
 
           ),
         ),
-
-
-
-
+        
       ],
     );
   }

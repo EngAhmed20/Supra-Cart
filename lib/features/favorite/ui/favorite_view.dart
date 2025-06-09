1a7d5a05245489371ccbdfc2d1fa608eb337a1c5
EngAhmed20 <ahmed2012muhammad@gmail.com>
2025-06-09 16:35:32 +0300

    store and fav ui

--- lib/features/favorite/ui/favorite_view.dart
+++ lib/features/favorite/ui/favorite_view.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../core/style/app_text_styles.dart';
+import '../../../core/widgets/product_list.dart';
 
 class FavoriteView extends StatelessWidget {
   const FavoriteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Favorite View',
-      ),
+    return ListView(
+      children: [
+        Text('Your Favorite Products',style: textStyle.Bold24,textAlign: TextAlign.center,),
+        SizedBox(height: 20.h,),
+        /// product List view
+        ProductList(),
+      ],
     );
+
   }
 }
--- lib/features/store/ui/store_view.dart
+++ lib/features/store/ui/store_view.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:supra_cart/core/style/app_text_styles.dart';
+import 'package:supra_cart/core/widgets/custom_search_filed.dart';
+import '../../../core/widgets/product_list.dart';
 class StoreView extends StatelessWidget {
-  const StoreView({super.key});
-
+  const  StoreView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Store View',
-      ),
+    return ListView(
+      children: [
+          Text('Welcome To Our Market',style: textStyle.Bold24,textAlign: TextAlign.center,),
+        SizedBox(height: 20.h,),
+        CustomSearchField(hintText: 'Search in Market',
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter a search term';
+            }
+            return null;
+          },
+          searchAction: (){
+
+          },),
+        SizedBox(height: 20.h,),
+        /// product List view
+        ProductList(),
+      ],
     );
   }
+
+
 }
+
+
+
+

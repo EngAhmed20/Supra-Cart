c0f08375bce780669316a78bb00df24f005c251b
EngAhmed20 <ahmed2012muhammad@gmail.com>
2025-06-10 15:38:13 +0300

    profile ui

--- lib/features/profile/ui/profile_view.dart
+++ lib/features/profile/ui/profile_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:supra_cart/core/style/app_colors.dart';
+import 'package:supra_cart/core/style/app_text_styles.dart';
+import 'package:supra_cart/features/profile/ui/widgets/custom_profile_button.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
@@ -6,8 +10,25 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        'Profile View',),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(radius: 50,backgroundColor: AppColors.kPrimaryColor,
+          child: Icon(Icons.person, size: 50, color: Colors.white),),
+           SizedBox(height: 10.h),
+          Text('User Name', style: textStyle.Bold19),
+          SizedBox(height: 5.h),
+          Text('Ahmed@gmail.com',style: textStyle.semiBold16,),
+          SizedBox(height: 20.h),
+          CustomProfileButton(title: 'Edit Name',icon: Icons.person,onTap: (){},),
+          SizedBox(height: 10.h),
+          CustomProfileButton(title: 'My Orders',icon: Icons.shopping_cart,onTap: (){},),
+          SizedBox(height: 10.h,),
+          CustomProfileButton(title: 'Logout',icon: Icons.logout,onTap: (){},),
+
+        ],
+      ),
     );
   }
 }
+
---
+++ lib/features/profile/ui/widgets/custom_profile_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/style/app_colors.dart';
+import '../../../../core/style/app_text_styles.dart';
+
+class CustomProfileButton extends StatelessWidget {
+  const CustomProfileButton({
+    super.key, required this.title, required this.icon, this.onTap,
+  });
+  final String title;
+  final IconData icon;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(icon,color: AppColors.kWhiteColor,size: 30,),
+      title: Text(title,textAlign: TextAlign.center, style: textStyle.Bold20),
+      textColor: AppColors.kWhiteColor,
+      trailing: const Icon(Icons.arrow_forward_ios,color: AppColors.kWhiteColor,size: 30,),
+      tileColor: AppColors.kPrimaryColor,
+      contentPadding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 5.h),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      onTap:onTap,
+    );
+  }
+}
